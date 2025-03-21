bfa644b0d0fcd9d16eb95c90bac3aa54bfa206de
Tania Lopes <taniainteractive@gmail.com>
2025-03-21 17:51:03 +0700

    Update frontend assets with new API metrics components

---
+++ frontend/dist/assets/unsupportedProp-dd492d88.js
@@ -0,0 +1,2 @@
+function u(o,e,t,p,n){const r=n||e;return typeof o[e]!="undefined"?new Error(`The prop \`${r}\` is not supported. Please remove it.`):null}export{u};
+//# sourceMappingURL=unsupportedProp-dd492d88.js.map
