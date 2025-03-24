4f9a58fd01ee97899180ca9047c5238ddf285369
Tânia Lopes <1516784+mctata@users.noreply.github.com>
2025-03-24 16:04:53 +0700

    Create campaign components index file

---
+++ frontend/src/components/campaigns/index.ts
@@ -0,0 +1,5 @@
+// Export all campaign components here
+export { default as AdSetDetail } from './AdSetDetail';
+export { default as AudienceSegment } from './AudienceSegment';
+export { default as LookalikeAudience } from './LookalikeAudience';
+export { default as AudienceOverlapAnalysis } from './AudienceOverlapAnalysis';
