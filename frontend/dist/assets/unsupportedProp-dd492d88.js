87d6b554d97551e731f0715d21f3fed31cf5218a
Tania Lopes <taniainteractive@gmail.com>
2025-03-28 11:13:38 +0700

    Build frontend for staging deployment

--- frontend/dist/assets/unsupportedProp-dd492d88.js
+++
@@ -1,2 +0,0 @@
-function u(o,e,t,p,n){const r=n||e;return typeof o[e]!="undefined"?new Error(`The prop \`${r}\` is not supported. Please remove it.`):null}export{u};
-//# sourceMappingURL=unsupportedProp-dd492d88.js.map
